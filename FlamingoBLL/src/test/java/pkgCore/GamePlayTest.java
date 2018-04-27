d7e239fa7f531b534b0af46611b9038deff2f9f7
Ken Chan <Ken Chan@128.4.78.136>
2018-04-26 23:44:08 -0400

    Second Commit

--- FlamingoBLL/src/test/java/pkgCore/GamePlayTest.java
+++ FlamingoBLL/src/test/java/pkgCore/GamePlayTest.java
@@ -17,23 +17,47 @@ public class GamePlayTest {
 				
 		Player p1 = new Player("Ken", 1);
 		Player p2 = new Player("Lissa", 2);
+		Player p3 = new Player("Jane", 3);
+		Player p4 = new Player("Doe", 4);
+		
 		Table t = new Table();
 		t.AddPlayerToTable(p1);
 		t.AddPlayerToTable(p2);
+		
+		Table t2 = new Table();
+		t.AddPlayerToTable(p3);
+		t.AddPlayerToTable(p4);
+		
 		t.CreateDeck(eGameType.BLACKJACK);
+		t2.CreateDeck(eGameType.BLACKJACK);
 		
 		ArrayList<Player> playerArray = new ArrayList<>();
 		playerArray.add(p1);
 		playerArray.add(p2);
 		
+		ArrayList<Player> playerArray2 = new ArrayList<>();
+		playerArray.add(p3);
+		playerArray.add(p4);
+		
 		GamePlayBlackJack gpbj = new GamePlayBlackJack(t);
 		
 		gpbj.AddPlayersToGame(playerArray);
-		
 		assertEquals(gpbj.GetPlayerInGame(p1), p1); 
+		assertEquals(gpbj.GetPlayerInGame(p2), p2); 
 		
 		gpbj.RemovePlayerFromGame(p1);
 		assertEquals(gpbj.GetPlayerInGame(p1), null);
+		gpbj.RemovePlayerFromGame(p2);
+		assertEquals(gpbj.GetPlayerInGame(p2), null);
+		
+		gpbj.AddPlayersToGame(playerArray2);
+		assertEquals(gpbj.GetPlayerInGame(p3), p3); 
+		assertEquals(gpbj.GetPlayerInGame(p4), p4); 
+		
+		gpbj.RemovePlayerFromGame(p3);
+		assertEquals(gpbj.GetPlayerInGame(p3), null);
+		gpbj.RemovePlayerFromGame(p4);
+		assertEquals(gpbj.GetPlayerInGame(p4), null);
 	}
 
 }
